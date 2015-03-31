ca92c34d84f8ae5292e7a874120b7c994bd6bae2
Zero Zhang <my7andriy@gmail.com>
2015-03-31 16:59:09 +0800

    register

--- src/Services/Registrar.php
+++ src/Services/Registrar.php
@@ -1,9 +1,10 @@
 <?php
 namespace Taketwo\Services;
 
-use Taketwo\Models\User;
 use Validator;
 use Illuminate\Contracts\Auth\Registrar as RegistrarContract;
+use Taketwo\Providers\TakeTwoUserProvider;
+use Taketwo\Models\User, Taketwo\Models\UserAuth;
 
 class Registrar implements RegistrarContract
 {
@@ -11,14 +12,13 @@ class Registrar implements RegistrarContract
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array $data
+     * @param  array $credentials
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    public function validator(array $data)
+    public function validator(array $credentials)
     {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
+        return Validator::make($credentials, [
+            'email' => 'required|email|max:255|unique:user_auth,identifier,NULL,id,type,email',
             'password' => 'required|confirmed|min:6',
         ]);
     }
@@ -26,16 +26,30 @@ class Registrar implements RegistrarContract
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array $data
+     * @param  array $credentials
      * @return User
      */
-    public function create(array $data)
+    public function create(array $credentials)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
+        $credentials = TakeTwoUserProvider::initCredentials($credentials);
 
+        $user = new User();
+        $user->username = $credentials['name'];
+        $user->email = '';
+//        $user->created_ip = \Request::getClientIp();
+//        $user->updated_ip = \Request::getClientIp();
+//        $user->status = User::STATUS_NORMAL;
+        $user->save();
+
+        /**
+         * add user_auth
+         */
+        $userAuth = new UserAuth();
+        $userAuth->user_id = $user->id;
+        $userAuth->type = $credentials['type'];
+        $userAuth->identifier = $credentials['identifier'];
+        $userAuth->credential = bcrypt($credentials['credential']);
+        $userAuth->save();
+        return $user;
+    }
 }
